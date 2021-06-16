aa42710330442ff93569d48281611205911aceb6
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-06-16 16:39:01 +0200

    1.0

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guest.welcome');
+});
+
+Auth::routes();
+
+// ROTTE AREA ADMIN
+// Route::get('/admin', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function () {
+        // ROTTA HOME Admin
+        Route::get('/', 'HomeController@index')->name('home');
+        // ROTTE RESOURCE POSTS
+    });
