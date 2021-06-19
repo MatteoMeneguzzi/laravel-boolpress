294ed80c0dd58235c41acd7c9ec88eecbc1668f9
MatteoMeneguzzi <meneguzzi.matteo16@gmail.com>
2021-06-19 18:32:54 +0200

    changes.3

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
         <header>
-            HEADER
+            <!-- <Header /> -->
+            Ciao
         </header>
 
         <main>
             <h1>Welcome to public home</h1>
         </main>
+
+        <p>Work in progress</p>
     </div>
 </template>
 
 <script>
-export default {
-    name: "App"
-};
+// import Header from "./components/Header.vue";
+// export default {
+//     name: "App",
+//     components: {
+//         Header
+//     }
+// };
 </script>
 
 <style lang="scss">
@@ -21,3 +28,17 @@ body {
     font-family: sans-serif;
 }
 </style>
+Already included file name '/Users/matteomeneguzzi/Boolean
+/laravel-boolpress/resources/js/components/Header.vue' differs from file name
+'/Users/matteomeneguzzi/Boolean
+/laravel-boolpress/resources/js/Components/Header.vue' only in casing. The file
+is in the program because: Imported via "./components/Header.vue" from file
+'/Users/matteomeneguzzi/Boolean /laravel-boolpress/resources/js/App.vue' Root
+file specified for compilation Root file specified for compilation Already
+included file name '/Users/matteomeneguzzi/Boolean
+/laravel-boolpress/resources/js/Components/Header.vue' differs from file name
+'/Users/matteomeneguzzi/Boolean
+/laravel-boolpress/resources/js/components/Header.vue' only in casing. The file
+is in the program because: Imported via "./Components/Header.vue" from file
+'/Users/matteomeneguzzi/Boolean /laravel-boolpress/resources/js/App.vue' Root
+file specified for compilation Root file specified for compilation
